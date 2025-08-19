0ccb6b3ec695278acc62f9ccbb866ddcf1531f59
suryawanshisambhaji9011-sudo <suryawanshisambhaji9011@gmail.com>
2025-08-19 06:58:20 +0530

    Create MainActivity.java

---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView tv = new TextView(this);
+        tv.setText("Hello, World!");
+        tv.setTextSize(24);
+        setContentView(tv);
+    }
+}
